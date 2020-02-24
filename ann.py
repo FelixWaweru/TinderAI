7e30ac019d312840c487ef6dea8266035beb5080
FelixWaweru <wawerufelix60@gmail.com>
2020-02-24 11:51:01 +0300

    Working AI

--- ann.py
+++ ann.py
@@ -1,32 +1,37 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Churn_Modelling.csv')
+dataset = pd.read_csv('swipe_data.csv')
 
-# Data in Column 3 to 12 are Independent Variables and thus wil be needed
-# Column are counted from 0 and we select from Column 3 to row 12(+1)
-X = dataset.iloc[:, 3:13].values
+# Data in Column 3 to 8 are Independent Variables and thus wil be needed
+# Column are counted from 0 and we select from Column 3 to row 8(+1)
+X = dataset.iloc[:, 3:9].values
 
 # The Column with results used to train the model (Column 13)
-y = dataset.iloc[:, 13].values
+y = dataset.iloc[:, 9].values
+
 
 
 # Encoding the Independent Variables(Assigning int values to text data in columns)
-# Encoder for the countries column(Columns counted from 0)
+# Encoder for the Job column(Columns counted from 0)
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X1 = LabelEncoder()
-X[:, 1] = labelencoder_X1.fit_transform(X[:, 1])
+X[:, 4] = labelencoder_X1.fit_transform(X[:, 4])
 
-# Encoder for the Gender column(Columns counted from 0)
+# Encoder for the School column(Columns counted from 0)
 labelencoder_X2 = LabelEncoder()
-X[:, 2] = labelencoder_X2.fit_transform(X[:, 2])
+X[:, 5] = labelencoder_X2.fit_transform(X[:, 5])
 
-# Creates Dummy variables for the country column
-onehotencoder = OneHotEncoder(categorical_features = [1])
+
+# Encoder for the Swipe column(Columns counted from 0)
+labelencoder_y1 = LabelEncoder()
+y = labelencoder_y1.fit_transform(y)
+
+onehotencoder = OneHotEncoder(categorical_features = [5])
 X = onehotencoder.fit_transform(X).toarray()
-# We drop column 0 by making X have column 1 to the last one(Prevent Dummy variable trap)
 X =X[:, 1:]
 
 
@@ -37,10 +42,10 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, rando
 # Feature Scaling
 # Feature scaling is used to convert variables to a uniform standard to prevent bias
 # Eg. The model might assume 3000km is larger than and 5Km before feature scaling
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
+# from sklearn.preprocessing import StandardScaler
+# sc = StandardScaler()
+# X_train = sc.fit_transform(X_train)
+# X_test = sc.transform(X_test)
 
 # Import Keras and the modules we will use in the ANN
 import keras
@@ -52,13 +57,12 @@ from keras.layers import Dense
 classifier = Sequential()
 
 # This is where we define and add the output and input layer
-
+# The output_dim is the number of nodes in the layer
 # The number of output_dim is roughly (input layers + 1) /2
+# init is used to initialize the weights
+# activation is the activation function used in the neuron
 # First Hidden layer i.e first neuron
-classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu', input_dim = 11))
-
-# Second hidden layer i.e second neuron
-classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu'))
+classifier.add(Dense(output_dim = 3, init = 'uniform', activation = 'relu', input_dim = 6))
 
 # Output layer
 # This layer will have only one output_dim since the output will be one value
@@ -67,18 +71,20 @@ classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu'))
 classifier.add(Dense(output_dim = 1, init = 'uniform', activation = 'sigmoid'))
 
 # Compiling the ANN
+# Initializes Stochastic Gradient Descent
 classifier.compile(optimizer = 'adam',loss = 'binary_crossentropy', metrics = ['accuracy'])
 
 #Fitting the ANN to the training set
-classifier.fit(X_train, y_train, batch_size = 10, epochs = 100)
+# The batch size is the number of turns after which to adjust the weights
+# After the weights have been adjusted for every row of data, that is called an epoch
+classifier.fit(X_train, y_train, batch_size = 10, epochs = 2000)
+
+classifier.save('TinderAIModel.h5')
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
 # We convert the data into true false statements
 y_pred = (y_pred>0.5)
-
-# Single customer Homework prediction
-homework_prediction = classifier.predict(np.array())
 
 # Making the Confusion Matrix
 # Used to check the accuracy out the ANN predictions
@@ -88,5 +94,15 @@ cm = confusion_matrix(y_test, y_pred)
 # Calculating the percentage accuracy of the model using data from the confusion matrix
 correct_train_data = cm[0][0]
 correct_test_data = cm[1][1]
-accuracy = ((correct_train_data + correct_test_data)/2000)*100
+accuracy = ((correct_train_data + correct_test_data)/40)*100
 print("The system is " + str(accuracy) + "% accurate.")
+
+# Plots a line graph showing correlations within the data
+sns.set_style("darkgrid")
+plt.xlabel("Age")
+plt.ylabel("Swipe Probability")
+# Sets the Range of ages in the graph between 18 and 30
+plt.xlim(18, 30)
+# Sets the Range of Swipe in the graph between 0.0 and 1.0
+plt.ylim(0.0, 1.0)
+sns.lineplot(X[:, 1], y, color='orange')
--- data_getter.py
+++ data_getter.py
@@ -1,177 +1,233 @@
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-import time
-import csv, keyboard
-from emoji import UNICODE_EMOJI
+import requests, secrets, tkinter, csv, urllib, io, PIL, keras
+from PIL import ImageTk
+import PIL.Image
+import numpy as np
+from tkinter import *
+from keras.models import load_model
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.get('https://tinder.com')
-loginUsername = "wawerufelix60@gmail.com"
-loginPassword = "xil015weru3d"
+# TODO: Update Gist with updated code
 
-
-def login():
-    driver.get('https://tinder.com')
-
-    time.sleep(15)
-
-    fb_btn = driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/div[2]/button')
-    fb_btn.click()
-
-    # switch to login popup
-    base_window = driver.window_handles[0]
-    driver.switch_to.window(driver.window_handles[1])
-
-    email_in = driver.find_element_by_xpath('//*[@id="email"]')
-    email_in.send_keys(loginUsername)
-
-    pw_in = driver.find_element_by_xpath('//*[@id="pass"]')
-    pw_in.send_keys(loginPassword)
-
-    login_btn = driver.find_element_by_xpath('//*[@id="u_0_0"]')
-    login_btn.click()
-    print("Logging in")
-
-    driver.switch_to.window(base_window)
-    time.sleep(5)
-
-    popup_1 = driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')
-    popup_1.click()
-
-    popup_2 = driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')
-    popup_2.click()
+tinderToken = secrets.AUTH_TOKEN
+TINDER_URL = "https://api.gotinder.com"
+TINDER_AUTH_URL = "/v2/auth/login/facebook"
+TINDER_PROFILES_URL = "/v2/recs/core"
 
 
-def filter(dataValue):
-    if "kilometers" in dataValue:
-        distanceVal = dataValue
+def get_info(data, swipeValue):
+    # Get the user ID
+    id = data['user']["_id"]
 
-    elif "kilometers" in dataValue:
-        distanceVal = dataValue
+    # Get the user name
+    name = data['user']['name']
 
-    elif dataValue == "Woman":
-        break
+    # Get the user Bio
+    bio = data['user']['bio']
 
-    elif "Lives" in dataValue:
-        break
-        
-    elif "kilometers" in dataValue:
-        distanceVal = dataValue
+    # Get the Bio length
+    bioTextLength = len(str(bio))
 
-def get_info():
-    time.sleep(10)
-    print("Getting Information")
-    # Click the more information button
-    moreInfo = driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[1]/div[3]/div[6]/button')
-    moreInfo.click()
+    # Get the user distance
+    distance = str(int(data["distance_mi"])*1.609) # Change distance to km
 
-    # get Name
-    print("Getting name")
-    name = driver.find_element_by_xpath \
-        ('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[2]/div[1]/div/div[1]/div/h1')
-    nameText = name.get_attribute('innerHTML')
-
-    # get Bio
-    print("Getting Bio")
+    # Get the user age
     try:
-        bio = driver.find_element_by_xpath \
-            ('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[2]/div[2]')
-        bioText = bio.get_attribute('innerHTML')
+        # Get first four digits(Year) in Json birthdate
+        birth_date = data['user']["birth_date"][:4]
+        age = str(2020 - int(birth_date))
     except:
-        bioText = ""
+        age = "0"
 
-    print("Getting Bio length")
-    bioTextLength = len(str(bioText))
+    numberOfPhotos = len(data['user']['photos'])
 
-    print("Getting bio emojis")
-    count = 0
-    if UNICODE_EMOJI in bioText.split():
-        bioTextEmoji = True
-    else:
-        bioTextEmoji = False
+    # Use Images in future AI
+    # images = list(map(lambda photo: photo["url"], data.get("photos", [])))
 
-    # get Age
-    print("Getting age")
-    age = driver.find_element_by_xpath \
-        ('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[2]/div[1]/div/div[1]/span')
-    ageText = age.get_attribute('innerHTML')
-
-
-
-    # get Distance
-    print("Getting distance")
-    distance = driver.find_element_by_xpath \
-        ('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[2]/div/div/div[2]/div[2]/div[2]')
-    distanceText = distance.get_attribute('innerHTML')
-    # get the numerical distance value
-    for s in distanceText.split():
-        if s.isdigit():
-            distanceText = s
-
-    # get Job
-    print("Getting Job")
-    job = driver.find_element_by_xpath \
-        ('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[2]/div[1]/div/div[2]/div[1]/div[2]')
-    jobText = job.get_attribute('innerHTML')
-    if str(jobText) == "":
-        jobText = False
-    else:
-        jobText = True
-
-    # get School
-    print("Getting school")
-    school = driver.find_element_by_xpath \
-        ('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[2]/div[1]/div/div[2]/div[1]/div[2]')
-    schoolText = school.get_attribute('innerHTML')
-    if str(schoolText) == "":
-        schoolText = False
-    else:
-        schoolText = True
-
-
-def like():
-    like_btn = driver.find_element_by_xpath(
-        '//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/button[3]')
-    like_btn.click()
-
-
-def dislike():
-    dislike_btn = driver.find_element_by_xpath(
-        '//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/button[1]')
-    dislike_btn.click()
-
-
-def start_swiping():
-    login()
-    print("Logged In")
+    # Get the user Job
     try:
-        info = get_info()
-        print("Begin button press")
-        if keyboard.is_pressed(''):
-            print("Like")
-            like()
-            swipeValue = "Like"
-            csvFile = open('swipe_data.csv', 'w', newline='')
-            object = csv.writer(csvFile)
-            object.writerow((info.nameText, info.bioText, info.bioTextLength, info.bioTextEmoji,
-                             info.ageText, info.distanceText, info.jobText, info.schoolText, swipeValue))
-            csvFile.close()
-            print("Data saved")
+        jobTitle = data['user']['jobs']
 
-        elif keyboard.is_pressed(''):
-            print("Dislike")
-            dislike()
-            swipeValue = "Dislike"
-            csvFile = open('swipe_data.csv', 'w', newline='')
-            object = csv.writer(csvFile)
-            object.writerow((info.nameText, info.bioText, info.bioTextLength, info.bioTextEmoji,
-                             info.ageText, info.distanceText, info.jobText, info.schoolText, swipeValue))
-            csvFile.close()
-            print("Data saved")
+    except:
+        jobTitle = "[]"
+
+    if str(jobTitle) == "[]":
+        job = False
+    else:
+        job = True
+
+    # Get the user School
+    try:
+     schoolTitle = data['user']['schools']
+    except:
+        schoolTitle = "[]"
+
+    if str(schoolTitle) == "[]":
+        school = False
+    else:
+        school = True
+
+    csvFile = open('swipe_data.csv', 'a', newline='')
+    object = csv.writer(csvFile)
+    object.writerow((id, name, bio.encode("utf-8"), age, numberOfPhotos, bioTextLength, distance, job, school, swipeValue))
+    csvFile.close()
+    print( str(name) + "'s Data saved")
+
+def like(data):
+    user_id = data['user']["_id"]
+    liked = requests.get(TINDER_URL + f"/like/{user_id}", headers={"X-Auth-Token": tinderToken}).json()
+    print("Liked")
+    get_info(data, "Like")
+
+def dislike(data):
+    user_id = data['user']["_id"]
+    liked = requests.get(TINDER_URL + f"/pass/{user_id}", headers={"X-Auth-Token": tinderToken}).json()
+    print("Disliked")
+    get_info(data, "Dislike")
+
+def startSwiping():
+    try:
+        print("Connecting to Tinder API")
+        print("Getting Nearby Users")
+        # get user profile info of people near me
+        data = requests.get(TINDER_URL + TINDER_PROFILES_URL, headers={"X-Auth-Token": tinderToken}).json()
+        try:
+            print("Connected successfully")
+            for user in data['data']['results']:
+                print(len(user['user']['photos']))
+                # Display the user pictures
+                for image in user['user']['photos']:
+                    mainWindow = tkinter.Tk()
+                    mainWindow.title("Profile Pictures")
+                    raw_data = urllib.request.urlopen(image['url']).read()
+                    im = PIL.Image.open(io.BytesIO(raw_data))
+                    image = ImageTk.PhotoImage(im.resize((400, 400)))
+                    label = Label(mainWindow, image=image)
+                    closeButton = Button(mainWindow, text = "Next" , command = mainWindow.destroy)
+                    label.pack()
+                    closeButton.pack()
+                    mainWindow.mainloop()
+
+                # Get first four digits(Year) in Json birthdate
+                birth_date = str(user['user']["birth_date"])[:4]
+                age = str(2020 - int(birth_date))
+
+                # Show user info in the terminal
+                print(user['user']['name'])
+                print("Bio: " + user['user']["bio"])
+                print("Age: " + age)
+                print("Distance: " + str(user["distance_mi"]))
+                print("Company: " + str(user['user']['jobs']))
+                print("School: " + str(user['user']['schools']))
+
+                # Get the user keypress. Q for dislike and E for like
+                choice = input("Begin button press: ")
+                if choice == 'e':
+                    like(user)
+                    print("Like")
+                elif choice == 'q':
+                    dislike(user)
+                    print("Dislike")
+                else:
+                    print("Invalid input")
+
+        except Exception as e:
+            print(str(e) + " Connection terminated. There's an error in the function.")
+
     except Exception as e:
-        print(e)
+        print(str(e) + " Connection failure")
 
 
 
-start_swiping()
+def TinderAI(school, age, numberOfPhotos, bioTextLength, distance, job):
+    classifier = load_model('TinderAIModel.h5')
+    # Single user Tinder prediction
+    swipePrediction = classifier.predict(np.array([[school, age, numberOfPhotos, bioTextLength, distance, job]]))
+    swipePrediction = (swipePrediction > 0.5)
 
+    return swipePrediction
+
+
+def AI_Swiper():
+    try:
+        print("Connecting to Tinder API")
+        print("Getting Nearby Users")
+        # get user profile info of people near me
+        data = requests.get(TINDER_URL + TINDER_PROFILES_URL, headers={"X-Auth-Token": tinderToken}).json()
+        try:
+            print("Connected successfully")
+            for user in data['data']['results']:
+
+                # Get first four digits(Year) in Json birthdate
+                birth_date = str(user['user']["birth_date"])[:4]
+                age = str(2020 - int(birth_date))
+
+                # Show user info in the terminal
+                print(user['user']['name'])
+                print("Bio: " + user['user']["bio"])
+                print("Age: " + age)
+                print("Distance: " + str(user["distance_mi"]))
+                print("Company: " + str(user['user']['jobs']))
+                print("School: " + str(user['user']['schools']))
+
+                # Get the user School
+                try:
+                    schoolTitle = user['user']['schools']
+                except:
+                    schoolTitle = "[]"
+
+                if str(schoolTitle) == "[]":
+                    school = 0
+                else:
+                    school = 1
+
+                # Get the user Bio
+                bio = user['user']['bio']
+
+                # Get the Bio length
+                bioTextLength = int(len(str(bio)))
+
+                # Get the user distance
+                distance = int(int(user["distance_mi"]) * 1.609)  # Change distance to km
+
+                # Get the user age
+                try:
+                    # Get first four digits(Year) in Json birthdate
+                    birth_date = user['user']["birth_date"][:4]
+                    age = int(2020 - int(birth_date))
+                except:
+                    age = 0
+
+                numberOfPhotos = len(user['user']['photos'])
+
+                # Use Images in future AI
+                # images = list(map(lambda photo: photo["url"], data.get("photos", [])))
+
+                # Get the user Job
+                try:
+                    jobTitle = user['user']['jobs']
+
+                except:
+                    jobTitle = "[]"
+
+                if str(jobTitle) == "[]":
+                    job = 0
+                else:
+                    job = 1
+                
+                choice = TinderAI(school, age, numberOfPhotos, bioTextLength, distance, job)
+                print(str(choice))
+                if str(choice) == '[[False]]':
+                    dislike(user)
+                    print("Dislike")
+                elif str(choice) == '[[ True]]':
+                    like(user)
+                    print("Like")
+                else:
+                    print("Invalid input")
+
+        except Exception as e:
+            print(str(e) + " Connection terminated. There's an error in the function.")
+
+    except Exception as e:
+        print(str(e) + " Connection failure")
+
+AI_Swiper()
